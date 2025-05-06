190ec944299578e26bb6308ae88cafa1f61c837f
Dhruvil Patel <pateldhruvilk2@gmail.com>
2025-05-06 12:05:22 +0530

    edit file

--- src/Componets/Singup/Singup.jsx
+++ src/Componets/Singup/Singup.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { isValidEmail, isValidPassword } from '../Validation/Validation';
-// import { isValidEmail, isValidPassword } from '../../Validation/Validation';
+import { isValidEmail,isValidPassword } from '../../Validation/Validation';
 import toast from 'react-hot-toast'
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebaseauth/Firebaseauth';
